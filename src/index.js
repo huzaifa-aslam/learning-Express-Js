6609ed38d709b1f2e7098e64e4d01807969d8b9f
Huzaifa Aslam <huzaifaslam45@gmail.com>
2021-03-22 23:47:39 +0500

    serve static file

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 const express = require('express');
+const path = require('path')
 const app = express()
+const staticPath = path.join(__dirname, '../public')
+app.use(express.static(staticPath));
+
 
 app.get('/', (req, res) => {
     res.send('hello from server')
