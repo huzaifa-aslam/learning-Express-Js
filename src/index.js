65f5072d582615bd56b0d32e26e8d0e669e44563
Huzaifa Aslam <huzaifaslam45@gmail.com>
2021-03-23 23:43:51 +0500

    added dynamic content

--- src/index.js
+++ src/index.js
@@ -1,14 +1,23 @@
 const express = require('express');
 const path = require('path')
 const app = express()
-const staticPath = path.join(__dirname, '../public')
-app.use(express.static(staticPath));
 
+const staticPath = path.join(__dirname, '../public')
+// app.use(express.static(staticPath));
+
+// template engine
+
+app.set("view engine", "hbs")
+app.get('', (req, res) => {
+    res.render("index", {
+        firstName: "Huzaifa"
+    })
+})
 
 app.get('/', (req, res) => {
     res.send('hello from server')
 })
 
-app.listen(8000, () => {
-    console.log("server listing at port 8000");
+app.listen(3000, () => {
+    console.log("server listing at port 3000");
 })
